dd84715bc68aff6581d4a21c825113db2db23f19
Evgeniya Dron <dron1920818@gmail.com>
2022-12-24 21:40:44 +0200

    2 commit test

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ print("Більше число - ", mane_function(a, b))
 
 
 def test_funcktion( b1, c1, d1):
+    if b1 > c1 and d1 > b1:
+        print("Умови виконуються")
+
+
+b = 6
+c = 4
+d = 64
+test_funcktion(b, c, d)
+
 
 
 
